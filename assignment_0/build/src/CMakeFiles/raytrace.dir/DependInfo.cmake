5bb193602c5a2068bc8c28f2e964009130071663
Ivan Snozzi <ivan.snozzi@epfl.ch>
2019-02-21 15:01:02 +0100

    green

---
+++ assignment_0/build/src/CMakeFiles/raytrace.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/snozzi/Desktop/Scuola/Computer Graphics/Project/IntroGraphics/assignment_0/src/Cylinder.cpp" "/home/snozzi/Desktop/Scuola/Computer Graphics/Project/IntroGraphics/assignment_0/build/src/CMakeFiles/raytrace.dir/Cylinder.cpp.o"
+  "/home/snozzi/Desktop/Scuola/Computer Graphics/Project/IntroGraphics/assignment_0/src/Image.cpp" "/home/snozzi/Desktop/Scuola/Computer Graphics/Project/IntroGraphics/assignment_0/build/src/CMakeFiles/raytrace.dir/Image.cpp.o"
+  "/home/snozzi/Desktop/Scuola/Computer Graphics/Project/IntroGraphics/assignment_0/src/Mesh.cpp" "/home/snozzi/Desktop/Scuola/Computer Graphics/Project/IntroGraphics/assignment_0/build/src/CMakeFiles/raytrace.dir/Mesh.cpp.o"
+  "/home/snozzi/Desktop/Scuola/Computer Graphics/Project/IntroGraphics/assignment_0/src/Plane.cpp" "/home/snozzi/Desktop/Scuola/Computer Graphics/Project/IntroGraphics/assignment_0/build/src/CMakeFiles/raytrace.dir/Plane.cpp.o"
+  "/home/snozzi/Desktop/Scuola/Computer Graphics/Project/IntroGraphics/assignment_0/src/Scene.cpp" "/home/snozzi/Desktop/Scuola/Computer Graphics/Project/IntroGraphics/assignment_0/build/src/CMakeFiles/raytrace.dir/Scene.cpp.o"
+  "/home/snozzi/Desktop/Scuola/Computer Graphics/Project/IntroGraphics/assignment_0/src/Sphere.cpp" "/home/snozzi/Desktop/Scuola/Computer Graphics/Project/IntroGraphics/assignment_0/build/src/CMakeFiles/raytrace.dir/Sphere.cpp.o"
+  "/home/snozzi/Desktop/Scuola/Computer Graphics/Project/IntroGraphics/assignment_0/src/raytrace.cpp" "/home/snozzi/Desktop/Scuola/Computer Graphics/Project/IntroGraphics/assignment_0/build/src/CMakeFiles/raytrace.dir/raytrace.cpp.o"
+  "/home/snozzi/Desktop/Scuola/Computer Graphics/Project/IntroGraphics/assignment_0/src/vec3.cpp" "/home/snozzi/Desktop/Scuola/Computer Graphics/Project/IntroGraphics/assignment_0/build/src/CMakeFiles/raytrace.dir/vec3.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/lodePNG/SYSTEM"
+  "../lib/lodePNG/."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/snozzi/Desktop/Scuola/Computer Graphics/Project/IntroGraphics/assignment_0/build/lib/lodePNG/CMakeFiles/lodePNG.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
