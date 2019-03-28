16b1dda12cca02899766581052805a2c402653a3
Boubacar Camara <bakarsadio@gmail.com>
2019-03-28 18:22:45 +0100

    Add textures

--- assignment_5/src/solar_viewer.cpp
+++ assignment_5/src/solar_viewer.cpp
@@ -412,7 +412,7 @@ void Solar_viewer::draw_scene(mat4& _projection, mat4& _view)
     sun_.tex_.bind();
     unit_sphere_.draw();
 
-
+	//render earth
 	m_matrix = mat4::translate(earth_.pos_) *mat4::rotate_y(earth_.angle_self_) * mat4::scale(earth_.radius_);
 	mv_matrix = _view * m_matrix;
 	mvp_matrix = _projection * mv_matrix;
@@ -424,6 +424,75 @@ void Solar_viewer::draw_scene(mat4& _projection, mat4& _view)
 	color_shader_.set_uniform("greyscale", (int)greyscale_);
 	earth_.tex_.bind();
 	unit_sphere_.draw();
+
+
+	//render stars
+	m_matrix = mat4::translate(stars_.pos_) *mat4::rotate_y(stars_.angle_self_) * mat4::scale(stars_.radius_);
+	mv_matrix = _view * m_matrix;
+	mvp_matrix = _projection * mv_matrix;
+	color_shader_.use();
+	color_shader_.set_uniform("modelview_projection_matrix", mvp_matrix);
+	color_shader_.set_uniform("t", sun_animation_time, true /* Indicate that time parameter is optional;
+															 it may be optimized away by the GLSL    compiler if it's unused. */);
+	color_shader_.set_uniform("tex", 0);
+	color_shader_.set_uniform("greyscale", (int)greyscale_);
+	stars_.tex_.bind();
+	unit_sphere_.draw();
+
+
+	//render mercury
+	m_matrix = mat4::translate(mercury_.pos_) *mat4::rotate_y(mercury_.angle_self_) * mat4::scale(mercury_.radius_);
+	mv_matrix = _view * m_matrix;
+	mvp_matrix = _projection * mv_matrix;
+	color_shader_.use();
+	color_shader_.set_uniform("modelview_projection_matrix", mvp_matrix);
+	color_shader_.set_uniform("t", sun_animation_time, true /* Indicate that time parameter is optional;
+															 it may be optimized away by the GLSL    compiler if it's unused. */);
+	color_shader_.set_uniform("tex", 0);
+	color_shader_.set_uniform("greyscale", (int)greyscale_);
+	mercury_.tex_.bind();
+	unit_sphere_.draw();
+
+	//render venus
+	m_matrix = mat4::translate(venus_.pos_) *mat4::rotate_y(venus_.angle_self_) * mat4::scale(venus_.radius_);
+	mv_matrix = _view * m_matrix;
+	mvp_matrix = _projection * mv_matrix;
+	color_shader_.use();
+	color_shader_.set_uniform("modelview_projection_matrix", mvp_matrix);
+	color_shader_.set_uniform("t", sun_animation_time, true /* Indicate that time parameter is optional;
+															 it may be optimized away by the GLSL    compiler if it's unused. */);
+	color_shader_.set_uniform("tex", 0);
+	color_shader_.set_uniform("greyscale", (int)greyscale_);
+	venus_.tex_.bind();
+	unit_sphere_.draw();
+
+	//render moon
+	m_matrix = mat4::translate(moon_.pos_) *mat4::rotate_y(moon_.angle_self_) * mat4::scale(moon_.radius_);
+	mv_matrix = _view * m_matrix;
+	mvp_matrix = _projection * mv_matrix;
+	color_shader_.use();
+	color_shader_.set_uniform("modelview_projection_matrix", mvp_matrix);
+	color_shader_.set_uniform("t", sun_animation_time, true /* Indicate that time parameter is optional;
+															 it may be optimized away by the GLSL    compiler if it's unused. */);
+	color_shader_.set_uniform("tex", 0);
+	color_shader_.set_uniform("greyscale", (int)greyscale_);
+	moon_.tex_.bind();
+	unit_sphere_.draw();
+
+	//render mars
+	m_matrix = mat4::translate(mars_.pos_) *mat4::rotate_y(mars_.angle_self_) * mat4::scale(mars_.radius_);
+	mv_matrix = _view * m_matrix;
+	mvp_matrix = _projection * mv_matrix;
+	color_shader_.use();
+	color_shader_.set_uniform("modelview_projection_matrix", mvp_matrix);
+	color_shader_.set_uniform("t", sun_animation_time, true /* Indicate that time parameter is optional;
+															 it may be optimized away by the GLSL    compiler if it's unused. */);
+	color_shader_.set_uniform("tex", 0);
+	color_shader_.set_uniform("greyscale", (int)greyscale_);
+	mars_.tex_.bind();
+	unit_sphere_.draw();
+	
+	
 
     /** \todo Render the star background, the spaceship, and the rest of the celestial bodies.
      *  For now, everything should be rendered with the color_shader_,
