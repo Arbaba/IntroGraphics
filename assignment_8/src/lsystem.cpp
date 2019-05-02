888dca5714671933a8bc9c7d5d8eb084936378da
snozzi <snozzi.ivan@gmail.com>
2019-05-02 10:38:20 +0200

    part 2 and 4

--- assignment_8/src/lsystem.cpp
+++ assignment_8/src/lsystem.cpp
@@ -25,11 +25,6 @@ std::string LindenmayerSystemDeterministic::expandSymbol(unsigned char const& sy
 		For a given symbol in the sequence, what should it be replaced with after expansion?
 		The rules are in this->rules, see lsystem.h for details.
 	*/
-	/*
-	if ((sym == '-') || (sym == '+') || (sym == ' ')){
-		return {char(sym)};
-	}
-	*/
 	auto mapped = this->rules.find(sym);
 
 	if (mapped != this->rules.end()) {
@@ -52,9 +47,7 @@ std::string LindenmayerSystem::expandOnce(std::string const& symbol_sequence) {
 		Perform one iteration of grammar expansion on `symbol_sequence`.
 		Use the expandSymbol method
 	*/
-
 	std::string out = "";
-	
 	
 	for (size_t i = 0 ; i < symbol_sequence.length(); i++){
     	out += expandSymbol(symbol_sequence[i]);
@@ -70,25 +63,12 @@ std::string LindenmayerSystem::expand(std::string const& initial, uint32_t num_i
 		TODO 1.3
 		Perform `num_iters` iterations of grammar expansion (use expandOnce)
 	*/
-
-	
 	std::string out = initial;
 
 	for (size_t i = 0; i < num_iters; i++){
 		out = expandOnce(out);
 	}
 	return out;
-	
-	std::cout << "NUMBER OF ITERATIONS: " << num_iters << "\n" << fflush;
-	std::cout << initial << "\n" << fflush;
-	
-	/*
-	if(num_iters <=0){
-		return initial;
-	}else{
-		expand(expandOnce(initial), num_iters - 1);
-	}
-   */
 	//============================================================
 }
 
@@ -99,9 +79,36 @@ std::vector<Segment> LindenmayerSystem::draw(std::string const& symbols) {
 		The initial position is (0, 0) and the initial direction is "up" (0, 1)
 		Segment is std::pair<vec2, vec2>
 	*/
-
+	std::vector<Segment> out;
+	std::vector<Segment> stack;
 	
-	return {};
+	vec2 p  = vec2(0, 0); 
+	vec2 f  = vec2(0, 1);
+
+	double angle = rotation_angle_deg * M_PI / 180 ;
+
+	for (size_t i = 0 ; i < symbols.length(); i++){
+    	if (symbols[i] == '+'){
+			f = vec2(f[0]*cos(angle) - f[1]*sin(angle), f[0]*sin(angle) + f[1]*cos(angle));
+
+		}else if (symbols[i] == '-'){
+			f = vec2(f[0]*cos(angle) + f[1]*sin(angle), f[1]*cos(angle) - f[0]*sin(angle));
+
+		}else if (symbols[i] == '['){
+			stack.push_back(std::make_pair(p, f));
+
+		}else if (symbols[i] == ']'){
+			p = stack.back().first;
+			f = stack.back().second;
+			stack.pop_back();
+			
+		}else{
+			out.push_back(std::make_pair(p, p+f));
+			p = p+f;
+		}
+	}
+
+	return out;
 	
 	//============================================================
 }
@@ -113,8 +120,22 @@ std::string LindenmayerSystemStochastic::expandSymbol(unsigned char const& sym) 
 		(stochastic case)
 		The rules are in this->rules, but now these are stochastic rules because this method belongs to the LindenmayerSystemStochastic class, see lsystem.h for details.
 	*/
-	return {char(sym)};
+	auto mapped = this->rules.find(sym);
 
+	double pr = this->dice.roll();
+
+	if (mapped != this->rules.end()){
+
+		for(std::vector<StochasticRule>::iterator it = mapped->second.begin(); it != mapped->second.end(); ++it) {
+			if(it->probability > pr){
+				return it->expansion;
+			}else{
+				pr = pr - it->probability;
+			}
+		}
+	}else{
+		return {char(sym)};
+	}
 	//============================================================
 }
 
